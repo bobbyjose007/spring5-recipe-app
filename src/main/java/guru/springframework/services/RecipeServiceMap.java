e3b9bd3640ea30d9367cefd99a2de5c098c753d8
bobbyjose007 <bobbyjose007@gmail.com>
2019-09-09 16:06:19 -0400

    Adding repositories

---
+++ src/main/java/guru/springframework/services/RecipeServiceMap.java
@@ -0,0 +1,33 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+
+import java.util.Set;
+
+public class RecipeServiceMap implements RecipeService {
+
+    @Override
+    public Set<Recipe> findAll() {
+        return super.f;
+    }
+
+    @Override
+    public Recipe findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Recipe save(Recipe object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Recipe object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
